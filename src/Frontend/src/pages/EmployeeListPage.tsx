9b31317ca10cf2dbeda07d55d83e9494d3df4319
Giorgio Corrizzato <78746079+DarkDragonLedas@users.noreply.github.com>
2025-03-05 17:56:23 +0000

    Created CustomerListPage and EmployeesListPage that show records in base of their endpoints

--- src/Frontend/src/pages/EmployeeListPage.tsx
+++ src/Frontend/src/pages/EmployeeListPage.tsx
@@ -1,5 +1,92 @@
-export default function EmployeeListPage() {
-    return <>
-        <i>Todo employees</i>
-    </>
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+    styled,
+    tableCellClasses,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+  
+interface EmployeesListQuery {
+    id: number;
+    code: string;
+    firstName: string;
+    lastName: string;
+    address: string;
+    email: string;
+    phone: string;
+    department?: 
+    {
+        code: string;
+        description: string;
+    }
 }
+
+export default function EmployeeListPage() {
+
+    const [list, setList] = useState<EmployeesListQuery[]>([]);
+    
+      useEffect(() => {
+        fetch("/api/employees/list")
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            setList(data as EmployeesListQuery[]);
+          });
+    }, []);
+
+    return (
+        <>
+          <Typography variant="h4" sx={{ textAlign: "center", mt: 4, mb: 4 }}>
+            Employees
+          </Typography>
+    
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableHeadCell>Code</StyledTableHeadCell>
+                  <StyledTableHeadCell>FirstName</StyledTableHeadCell>
+                  <StyledTableHeadCell>LastName</StyledTableHeadCell>
+                  <StyledTableHeadCell>Address</StyledTableHeadCell>
+                  <StyledTableHeadCell>Email</StyledTableHeadCell>
+                  <StyledTableHeadCell>Phone</StyledTableHeadCell>
+                  <StyledTableHeadCell>Department-Code</StyledTableHeadCell>
+                  <StyledTableHeadCell>Department-Description</StyledTableHeadCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {list.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>{row.code}</TableCell>
+                    <TableCell>{row.firstName}</TableCell>
+                    <TableCell>{row.lastName}</TableCell>
+                    <TableCell>{row.address}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                    <TableCell>{row.phone}</TableCell>
+                    <TableCell>{row.department?.code}</TableCell>
+                    <TableCell>{row.department?.description}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+    );
+}
+
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.primary.light,
+      color: theme.palette.common.white,
+    },
+}));
